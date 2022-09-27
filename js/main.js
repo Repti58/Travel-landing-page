d38ae3b31c76172ef5ddd58819660de8ffae95a2
repti85 <ids-reptile@bk.ru>
2022-09-28 01:48:27 +0300

    add burger menu

--- js/main.js
+++ js/main.js
@@ -10,4 +10,21 @@ window.onscroll = () => {
         headerWrapper.classList.remove('header__wrapper_active')
     }
 }
+}());
+
+(function () {
+const burgerItem = document.querySelector('.burger')
+const menu = document.querySelector('.header__nav')
+const close = document.querySelector('.header__nav-close')
+
+burgerItem.addEventListener ('click', () => {
+    menu.classList.add('header__nav-active-transition')
+    menu.classList.add('header__nav-active')
+
+})
+close.addEventListener ('click', () => {
+    menu.classList.remove('header__nav-active')
+    menu.classList.remove('header__nav-active-transition')
+})
+
 }())
